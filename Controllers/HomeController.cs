6e5ec2c5c3a6270cdceff35b7dee30d9b8f8d247
Roni Kurniawan <roni.kurniawan@outlook.com>
2023-06-21 12:42:43 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using DemoAspNetCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+
+namespace DemoAspNetCore.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly GraphServiceClient _graphServiceClient;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, GraphServiceClient graphServiceClient)
+        {
+            _logger = logger;
+            _graphServiceClient = graphServiceClient; ;
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "MicrosoftGraph:Scopes")]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["DisplayName"] = user.DisplayName;
+            ViewData["Mail"] = user.Mail;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
